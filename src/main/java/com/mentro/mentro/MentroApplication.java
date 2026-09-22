4af97a3dfb78c6165f9a25cd6618beb8d6d30c12
himbukharidev <bilalbukharimillionaire@gmail.com>
2026-09-22 09:13:46 -0700

    Initialize the project

---
+++ src/main/java/com/mentro/mentro/MentroApplication.java
@@ -0,0 +1,14 @@
+package com.mentro.mentro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentroApplication.class, args);
+		System.out.println("NAchiooooooooo");
+	}
+
+}
---
+++ src/test/java/com/mentro/mentro/MentroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentro.mentro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
